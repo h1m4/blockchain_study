0251266b085c53054e86449459b799c63c1a953e
HIMA <mjmjandk@naver.com>
2019-01-22 21:18:45 +0900

    0122

--- blockchain_실습/이더리움-부동산-dapp/real-estate/contracts/RealEstate.sol
+++ blockchain_실습/이더리움-부동산-dapp/real-estate/contracts/RealEstate.sol
@@ -10,7 +10,9 @@ contract RealEstate {
  address public owner;
  address[10] public buyers;
 
- event LogBuyRealEstates(
+
+
+ event LogBuyRealEstate(
      address _buyer,
      uint _id
  );
@@ -24,7 +26,16 @@ contract RealEstate {
 
 
      owner.transfer(msg.value); // 함수로 넘어온 이더
+     emit LogBuyRealEstate(msg.sender, _id);
 
-     
+
+ }
+
+ function getBuyerInfo(uint _id) public view returns (address, bytes32, uint){
+     Buyer memory buyer = buyerInfo[_id];
+     return (buyer.buyerAddress, buyer.name, buyer.age);
+ }
+ function getAllBuyers() public view returns (address[10]){
+     return buyers;
  }
 }
